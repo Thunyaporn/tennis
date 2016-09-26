32e0cee60974b1cf97c27af3fa09661b4369c189
Thunyaporn <thunthunyaporn@gmail.com>
2016-09-27 05:33:42 +0700

    Update tennisgame.spec.js

--- tennisgame.spec.js
+++ tennisgame.spec.js
@@ -1,16 +1,31 @@
+
 function Tennies(player){
 
-  this.scoreA = 0;
-  this.scoreB = 0;
 
-  if( player === 1){
+      this.scoreA = 0;
+      this.scoreB = 0;
+
+
+
+ if(player === 1){
       this.scoreA += 15;
-      return "( " + this.scoreA + " , " + this.scoreB + " )";
+
+      return "( " + this.scoreA + " , " + this.scoreB + " )" + "--->" + Score( this.scoreA , this.scoreB);
 
   }
-  else{
-       this.scoreB += 15;
-       return  "( " + this.scoreA + " , " + this.scoreB  + " )";
-  }
 
+
+  else if(player === 2){
+
+      this.scoreB += 15;
+
+      return "( " + this.scoreA + " , " + this.scoreB + " )" + "--->" + Score( this.scoreA , this.scoreB);
+  }
+}
+
+function Score( scoreA , scoreB ){
+
+  if( scoreA === 0 &&scoreB === 0){
+     return " Love Love ";
+  }
 }
